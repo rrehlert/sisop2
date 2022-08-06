77f1e5b2db10ddb4127f948578201e62b603ffcd
wpramio <willianpramio@hotmail.com>
2022-08-06 13:08:50 -0300

    feat: add 'missed_calls' attr and methods to Machine class

--- machine.cpp
+++ machine.cpp
@@ -4,6 +4,7 @@
 
 #define AWAKE 1
 #define SLEEPING 0
+#define MAX_MISSED_CALLS 3
 
 using namespace std;
 
@@ -13,6 +14,7 @@ class Machine {
 		string host_name;
   	string IP_addr;
   	int status;
+		unsigned int missed_calls;
 
   public:
 		Machine(){};
@@ -21,12 +23,14 @@ class Machine {
 			host_name = hostname;
 			IP_addr = IP;
 			status = AWAKE;
+			missed_calls = 0;
 		}
 
 		Machine(const Machine& m1) {
 			host_name = m1.host_name;
 			IP_addr = m1.IP_addr;
 			status = m1.status;
+			missed_calls = m1.missed_calls;
 		}
 
 		int getStatus() {
@@ -61,14 +65,29 @@ class Machine {
 			host_name = new_host_name;
 		}
 
+		unsigned int getMissedCalls() {
+			return missed_calls;
+		}
+
+		void increaseMissedCalls() {
+			missed_calls += 1;
+			if (missed_calls >= MAX_MISSED_CALLS)
+				status = SLEEPING;
+		}
+
+		void resetMissedCalls() {
+			missed_calls = 0;
+		}
+
 		bool isAwaken() {
 			return (status == AWAKE);
 		}
 
 		void print() {
-			cout << "::Machine::" << endl;
-			cout << "IP: " << IP_addr << endl;
-			cout << "Hostname: " << host_name << endl;
-			cout << "Status: " << (status == AWAKE ? "awake" : "sleeping") << endl;
+			cout << "::Machine::" << '\n';
+			cout << "IP: " << IP_addr << '\n';
+			cout << "Hostname: " << host_name << '\n';
+			cout << "Status: " << (status == AWAKE ? "awake" : "sleeping") << '\n';
+			cout << "Missed calls: " << missed_calls << endl;
 		}
 };
