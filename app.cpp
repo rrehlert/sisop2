79f5227b3a1dae2b3f652002757343b6efd73330
wpramio <willianpramio@hotmail.com>
2022-08-06 13:14:35 -0300

    feat: initial logic to set and reset missed_calls on Machines

--- app.cpp
+++ app.cpp
@@ -26,7 +26,6 @@ int main(int argc, char *argv[]) {
 	char buffer[256];
 	int broadcastPermission = 1;
   bool manager = false;
-	string hostname;
 	map<string, Machine> machines;
 
 	if (argc > 1 && (string)argv[1] == "manager") {
@@ -49,10 +48,6 @@ int main(int argc, char *argv[]) {
 		cout << "Role: [P]" << endl;
     if ((ptcp_sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
       cerr << "[P] ERROR opening socket" << endl;
-		if (setsockopt(ptcp_sock_fd, SOL_SOCKET, SO_BROADCAST, &broadcastPermission, sizeof(broadcastPermission)) < 0){
-      cerr << "[P] setsockopt error" << endl;
-      exit(1);
-    }
 
     ptcp_addr.sin_family = AF_INET;
     ptcp_addr.sin_port = htons(PORT);
@@ -64,6 +59,11 @@ int main(int argc, char *argv[]) {
 
 	while(true) {
     if (manager == true) {
+
+			// Aumenta o counter de chamadas de todas Machines
+			for (auto it = machines.begin(); it != machines.end(); ++it)
+					it->second.increaseMissedCalls();
+
 	  	// Envia pacote a procura de participantes acordados
       send_res = sendto(mng_sock_fd, "Are you awake?\n", 15, 0, (const struct sockaddr *) &broadcast_addr, sizeof(struct sockaddr_in));
 		  if (send_res < 0)
@@ -72,33 +72,43 @@ int main(int argc, char *argv[]) {
       // Recebe resposta dos participantes
       // TODO: receber as respostas (que podem vir ao mesmo tempo) em ordem de chegada
       // NENHUMA mensagem deve ser perdida
-      recv_res = recvfrom(mng_sock_fd, buffer, 256, 0, (struct sockaddr *) &ptcp_addr, &length);
-      if (recv_res < 0)
-        cerr << "[M] ERROR recvfrom" << endl;
-			else
+      recv_res = recvfrom(mng_sock_fd, buffer, 256, MSG_DONTWAIT, (struct sockaddr *) &ptcp_addr, &length);
+      if (recv_res < 0) {
+        cerr << "[M] Nobody answered" << endl;
+			}
+			else {
 				cout << "[M] Participant answered: " << buffer;
 
-			host_struct = gethostbyaddr(&(ptcp_addr.sin_addr), sizeof(ptcp_addr.sin_addr), AF_INET);
-			if (host_struct == NULL) {
-				cerr << "[M] Couldn't get host name, using IP" << endl;
-				hostname = inet_ntoa(ptcp_addr.sin_addr);
-			}
-			else {
-				hostname = host_struct->h_name;
-			}
+				string IP_addr = inet_ntoa(ptcp_addr.sin_addr);
+				string hostname;
 
-			Machine mach(inet_ntoa(ptcp_addr.sin_addr), hostname);
+				host_struct = gethostbyaddr(&(ptcp_addr.sin_addr), sizeof(ptcp_addr.sin_addr), AF_INET);
+				if (host_struct == NULL) {
+					cerr << "[M] Couldn't get host name, using IP" << endl;
+					hostname = IP_addr;
+				}
+				else {
+					hostname = host_struct->h_name;
+				}
 
-			if ( machines.count(mach.getIP()) > 0)
-   			cout << "Machine is known \n" << endl;
-			else {
-   			cout << "Machine is unknown! Adding to the map \n" << endl;
-				machines[mach.getIP()] = mach;
+				// Looking for the machine on the map
+				if ( machines.count(IP_addr) > 0) {
+					cout << "Machine is known \n" << endl;
+					auto it = machines.find(IP_addr);
+					it->second.resetMissedCalls();
+					it->second.setAwake();
+				}
+				else {
+					cout << "Machine is unknown! Adding to the map \n" << endl;
+					Machine mach(IP_addr, hostname);
+					machines[IP_addr] = mach;
+				}
+
 			}
 
 			cout << "** Machines Map **" << endl;
 			for (auto it = machines.begin(); it != machines.end(); ++it) {
-    		cout << it->first << " => " << endl;
+				cout << it->first << " => " << endl;
 				it->second.print();
 				cout << '\n';
 			}
