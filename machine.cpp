31b0bcd2304299845a2736b8d4616b8a834e1e89
wpramio <willianpramio@hotmail.com>
2022-08-06 11:36:33 -0300

    feat: add methods and attributes to Machine class

--- machine.cpp
+++ machine.cpp
@@ -1,22 +1,74 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <list>
-#include <netinet/in.h>
+
+#define AWAKEN 1
+#define SLEEPY 0
 
 using namespace std;
 
-class Machine{
+class Machine {
 
-    private:
-        string IPaddr;
-        int status;
-        
+	private:
+		string host_name;
+  	string IP_addr;
+  	int status;
 
-    public:
+  public:
+		Machine(){};
 
+		Machine(string IP, string hostname) {
+			host_name = hostname;
+			IP_addr = IP;
+			status = AWAKEN;
+		}
 
+		Machine(const Machine& m1) {
+			host_name = m1.host_name;
+			IP_addr = m1.IP_addr;
+			status = m1.status;
+		}
 
+		int getStatus() {
+			return status;
+		}
 
+		void setStatus(const int new_status) {
+			status = new_status;
+		}
 
-}
+		void setAwaken() {
+			status = AWAKEN;
+		}
+
+		void setSleepy() {
+			status = SLEEPY;
+		}
+
+		string getIP() {
+			return IP_addr;
+		}
+
+		void setIP(const string new_IP) {
+			IP_addr = new_IP;
+		}
+
+		string getHostname() {
+			return host_name;
+		}
+
+		void setHostname(const string new_host_name) {
+			host_name = new_host_name;
+		}
+
+		bool isAwaken() {
+			return (status == AWAKEN);
+		}
+
+		void print() {
+			cout << "::Machine::" << endl;
+			cout << "IP: " << IP_addr << endl;
+			cout << "Hostname: " << host_name << endl;
+			cout << "Status: " << (status == AWAKEN ? "awaken" : "sleepy") << endl;
+		}
+};
